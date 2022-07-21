ec61f5f056153943340bb2128c275df7a5c3888b
ChaeYubin <63189595+ChaeYubin@users.noreply.github.com>
2022-07-21 22:47:15 +0900

    추가 : 상단 메뉴바 구성중

--- app/src/main/java/com/example/soundy/MainActivity.kt
+++ app/src/main/java/com/example/soundy/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.soundy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnGoLogin : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGoLogin = findViewById(R.id.btnGoLogin)
+
+        btnGoLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
