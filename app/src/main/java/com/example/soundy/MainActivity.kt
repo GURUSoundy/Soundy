e151766da24902110881bff7c1948e40a5022139
yejin <bijou__ye@naver.com>
2022-07-25 01:06:26 +0900

    추가: 회원가입 페이지 이동

--- app/src/main/java/com/example/soundy/MainActivity.kt
+++ app/src/main/java/com/example/soundy/MainActivity.kt
@@ -7,15 +7,23 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnGoLogin : Button
+    lateinit var btnGoSignUp : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnGoSignUp =findViewById(R.id.btnGoSignUp)
         btnGoLogin = findViewById(R.id.btnGoLogin)
 
         btnGoLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        /* 회원가입 버튼 */
+        btnGoSignUp.setOnClickListener {
+            val intent = Intent(this, signUp::class.java)
             startActivity(intent)
         }
     }
