177283d5e49cac45c208059c80762c84b0b4f9c6
ChaeYubin <63189595+ChaeYubin@users.noreply.github.com>
2022-07-26 11:38:48 +0900

    추가 : 디렉토리 클릭 시 녹음 화면 액티비티로 넘어가기 위한 테스트 코드

--- app/src/main/java/com/example/soundy/DirectoryAdapter.kt
+++ app/src/main/java/com/example/soundy/DirectoryAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
@@ -12,7 +13,17 @@ class DirectoryAdapter(val directoryList: ArrayList<Directorys>) : RecyclerView.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DirectoryAdapter.CustomViewHolder {
         /* dir_list_item 화면 inflate */
         val view = LayoutInflater.from(parent.context).inflate(R.layout.dir_list_item, parent, false)
-        return CustomViewHolder(view)
+        return CustomViewHolder(view).apply {
+            /* 아이템(디렉토리) 클릭 이벤트 리스너 */
+            itemView.setOnClickListener {
+                /* 클릭된 아이템의 포지션 */
+                val curPos : Int = adapterPosition
+                /* curPos에 위치한 아이템 가져오기 */
+                val directory: Directorys = directoryList.get(curPos)
+                /* 테스트용 토스트 메시지 (추후 액티비티 전환으로 수정 예정) parent.context->FileListActivity */
+                Toast.makeText(parent.context, "디렉토리 이름: ${directory.dirName}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun getItemCount(): Int {
